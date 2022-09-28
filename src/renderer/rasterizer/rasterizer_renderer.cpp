a62c3024959c795e41c0dc698217459cfb476af1
al1ych <decter49pro@gmail.com>
2022-09-28 11:57:25 +0300

    update raterizer_renderer.cpp

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -6,14 +6,15 @@
 void cg::renderer::rasterization_renderer::init()
 {
 	rasterizer = std::make_shared<
-	        cg::renderer::rasterizer<
-	                cg::vertex, cg::unsigned_color>>();
+			cg::renderer::rasterizer<
+					cg::vertex, cg::unsigned_color>>();
 	rasterizer->set_viewport(settings->width, settings->height);
 	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
-			settings->width, settings->height
-			);
+			settings->width, settings->height);
 	rasterizer->set_render_target(render_target);
-	// TODO Lab: 1.03 Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
+
+	model = std::make_shared<cg::world::model>();
+	model->load_obj(settings->model_path);
 	// TODO Lab: 1.04 Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
 	// TODO Lab: 1.06 Add depth buffer in `cg::renderer::rasterization_renderer`
 }
@@ -24,7 +25,13 @@ void cg::renderer::rasterization_renderer::render()
 
 	// TODO Lab: 1.04 Implement `vertex_shader` lambda for the instance of `cg::renderer::rasterizer`
 	// TODO Lab: 1.05 Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
-	// TODO Lab: 1.03 Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
+
+	for (size_t shape_id = 0; shape_id < model->get_index_buffers().size(); shape_id++)
+	{
+		rasterizer->set_vertex_buffer(model->get_vertex_buffers()[shape_id]);
+		rasterizer->set_index_buffer(model->get_index_buffers()[shape_id]);
+		rasterizer->draw(model->get_index_buffers()[shape_id]->get_number_of_elements(), 0);
+	}
 }
 
 void cg::renderer::rasterization_renderer::destroy() {}
